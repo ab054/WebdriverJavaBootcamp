b640823379e7e6ebaed8df582f39d2cf1c934731
ab054 <alfamiliya@gmail.com>
2018-09-20 21:52:39 -0700

    day2 complete

--- src/test/java/day2/Class1.java
+++ src/test/java/day2/Class1.java
@@ -1,6 +1,7 @@
 package day2;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
 
@@ -8,6 +9,7 @@ public class Class1 {
 
     @Test
     public void test001() throws Exception {
+        System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "/src/test/resources/chromedriver");
         System.setProperty("webdriver.gecko.driver", System.getProperty("user.dir") + "/src/test/resources/geckodriver");
 
         WebDriver driver = new FirefoxDriver();
