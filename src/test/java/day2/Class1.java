996cfe719257b89e6a77d73b78d088c9d6428e8f
ab054 <alfamiliya@gmail.com>
2018-09-22 12:28:21 -0700

    day2 progress

--- src/test/java/day2/Class1.java
+++ src/test/java/day2/Class1.java
@@ -1,4 +1,28 @@
 package day2;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
 public class Class1 {
+
+    @Test
+    public void test001(){
+        System.setProperty("webdriver.gecko.driver", "/Users/abarabash/Documents/MyProject/WebdriverJavaBootcamp/src/test/resources/geckodriver");
+
+        WebDriver driver = new FirefoxDriver();
+        driver.get("http://google.com");
+    }
+
+    @Test
+    public void testInterface() throws Exception {
+        Selenium driver = new SeleniumAleksDriver();
+        //todo
+        Selenium driverMine = new SeleniumMyOwnDriver();
+        driver.get();
+        driver.anotherMethod();
+
+        driverMine.anotherMethod();
+        driverMine.get();
+    }
 }
---
+++ src/test/java/day2/SeleniumAleksDriver.java
@@ -0,0 +1,23 @@
+package day2;
+
+interface Selenium {
+  void get();
+  void anotherMethod();
+}
+
+
+public class SeleniumAleksDriver implements Selenium {
+    public void get() {
+        System.out.println("Implementation of get() method");
+    }
+
+    public void anotherMethod() {
+        System.out.println("Implementation of anotherMethod() method");
+    }
+}
+
+class Example {
+    void test() {
+        Selenium driver = new SeleniumAleksDriver();
+    }
+}
