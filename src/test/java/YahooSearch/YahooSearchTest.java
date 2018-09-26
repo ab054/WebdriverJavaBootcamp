76f48cc7e2e5db87539374784b646b8ca4b52509
Alex <alfamiliya@gmail.com>
2018-09-25 21:48:38 -0700

    annotations added

--- src/test/java/YahooSearch/YahooSearchTest.java
+++ src/test/java/YahooSearch/YahooSearchTest.java
@@ -1,15 +1,47 @@
 package YahooSearch;
 
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 public class YahooSearchTest {
 
+    private WebDriver driver;
+
+    @BeforeTest
+    public void setupDriver() {
+        System.setProperty("webdriver.gecko.driver", System.getProperty("user.dir") + "/src/test/resources/geckodriver.exe");
+        driver = new FirefoxDriver();
+    }
+
+    @AfterTest
+    public void closeDrive() {
+        driver.quit();
+    }
+
     @Test
     public void testSearch() throws Exception {
-//        navigatingToMainPage();
-//        typeRequestInSearchInput();
-//        submitSearch();
-//        printOutAmountOfResult();
+        navigatingToMainPage();
+        typeRequestInSearchInput();
+        submitSearch();
+        printOutAmountOfResult();
     }
+
+    private void typeRequestInSearchInput() {
+    }
+
+    private void navigatingToMainPage() {
+        driver.get("https://yahoo.com");
+    }
+
+    private void submitSearch() {
+    }
+
+    private void printOutAmountOfResult() {
+    }
+
+
 }
